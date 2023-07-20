58efa175a6bc1d12144531f8199d1073194a842d
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-20 14:59:58 +0100

    Proof of concept using a toml config file to drive argparse within the Pipeline class
    Requires https://github.com/histofyi/pipeline/commit/8081faf57892134dec8acffbd4de1a9f69d768c4
    
    or higher

--- steps/run_pipeline.py
+++ steps/run_pipeline.py
@@ -6,66 +6,22 @@ from pipeline import Pipeline
 
 from steps import steps
 
+import toml
 
 def run_pipeline(**kwargs) -> Dict:
-    pipeline = Pipeline(**kwargs)
+    pipeline = Pipeline()
+
+    kwargs = pipeline.parse_cli_args()
+
+    print (kwargs)
+    pipeline.say_hello()
 
 
 def main():
 
-    mhc_class = None
+    
 
-    parser = argparse.ArgumentParser(prog='Structure Pipeline',
-                    description='This pipeline builds a dataset of information about 3D structures.',
-                    epilog='For more information see - https://github.com/histofyi/structure_pipeline')    
-
-    arguments = [{
-            'flag':'v',
-            'variable_name':'verbose',
-            'help':'increases output verbosity (non-verbosity is the default)',
-            'action':'store_true',
-            'default':False
-        },
-        {
-            'flag':'f',
-            'variable_name':'force',
-            'help':'forces reloading of underlying datasets (not forcing reload is the default)',
-            'action':'store_true',
-            'default':False
-        },
-        {
-            'flag':'r',
-            'variable_name':'release',
-            'help':'switch between development and release modes (development mode is the default)',
-            'action':'store_true',
-            'default':False
-        },
-        {
-            'flag':'a',
-            'variable_name':'all_data',
-            'help':'whether all data should be processed, or only new structures',
-            'action':'store_true',
-            'default':False
-        },
-        {
-            'flag':'m',
-            'variable_name':'mhc_class',
-            'help':'switch the pipeline between MHC Class I and Class II, currently only Class I to which it defaults',
-            'action':'store',
-            'default':'class_i'
-        }
-        
-    ]
-
-    for argument in arguments:
-        parser.add_argument(f"-{argument['flag']}", f"--{argument['variable_name']}", help=argument['variable_name'], action=argument['action'])
-
-    defaults = {argument['variable_name']:argument['default'] for argument in arguments}
-    parser.set_defaults(**defaults)
-
-    kwargs = vars(parser.parse_args())
-
-    output = run_pipeline(**kwargs)
+    output = run_pipeline()
 
 
 if __name__ == '__main__':
