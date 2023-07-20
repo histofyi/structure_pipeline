fc1357438ca2223b47b27c1a39939403e687d21d
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-20 11:22:24 +0100

    Initial boilerplate for running the pipeline

---
+++ steps/run_pipeline.py
@@ -0,0 +1,16 @@
+from typing import Dict
+
+from pipeline import Pipeline
+
+from steps import steps
+
+def run_pipeline() -> Dict:
+    pipeline = Pipeline()
+
+
+def main():
+    run_pipeline()
+
+
+if __name__ == '__main__':
+    main()
