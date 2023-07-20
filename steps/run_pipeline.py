c3eb6cacec1f7916193f872a58c33ad277fa702d
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-20 13:54:01 +0100

    Refactored to form a prototype of how the argument parsing could be driven by configuration

--- steps/run_pipeline.py
+++ steps/run_pipeline.py
@@ -1,62 +1,71 @@
 from typing import Dict
 
-import argparse
+import argparse, configparser
 
 from pipeline import Pipeline
 
 from steps import steps
 
-# Constants. These should be in config, but that's not loaded until after the instantiation of the Pipeline object, so these are hard coded as an exception
-default_mhc_class = 'class_i'
-default_data_level = 'new_only'
 
-
-def run_pipeline(verbose:bool=False, force:bool=False, mode:bool='development', mhc_class:str=default_mhc_class, all_data:str=default_data_level) -> Dict:
-    pipeline = Pipeline()
+def run_pipeline(**kwargs) -> Dict:
+    pipeline = Pipeline(**kwargs)
 
 
 def main():
+
+    mhc_class = None
+
     parser = argparse.ArgumentParser(prog='Structure Pipeline',
                     description='This pipeline builds a dataset of information about 3D structures.',
-                    epilog='For more information see - https://github.com/histofyi/structure_pipeline')
-    parser.add_argument('-v','--verbose', help='increases output verbosity (non-verbosity is the default)', action='store_true')
-    parser.add_argument('-f', '--force', help='forces reloading of underlying datasets (not forcing reload is the default)', action='store_true')
-    parser.add_argument('-r', '--release', help='switch between development and release modes (development mode is the default)', action='store_true')
-    parser.add_argument('-a', '--all_data', help='whether all data should be processed, or only new structures', action='store_true')
-    parser.add_argument('-m', '--mhc_class', help='switch the pipeline between MHC Class I and Class II, currently only Class I to which it defaults')
-    args = parser.parse_args() 
+                    epilog='For more information see - https://github.com/histofyi/structure_pipeline')    
 
-    if args.verbose:
-        verbose = True
-    else:
-        verbose = False
-    if args.force:
-        force = True
-    else:
-        force = False
-    if args.release:
-        mode = 'release' 
-    else:
-        mode = 'development'    
+    arguments = [{
+            'flag':'v',
+            'variable_name':'verbose',
+            'help':'increases output verbosity (non-verbosity is the default)',
+            'action':'store_true',
+            'default':False
+        },
+        {
+            'flag':'f',
+            'variable_name':'force',
+            'help':'forces reloading of underlying datasets (not forcing reload is the default)',
+            'action':'store_true',
+            'default':False
+        },
+        {
+            'flag':'r',
+            'variable_name':'release',
+            'help':'switch between development and release modes (development mode is the default)',
+            'action':'store_true',
+            'default':False
+        },
+        {
+            'flag':'a',
+            'variable_name':'all_data',
+            'help':'whether all data should be processed, or only new structures',
+            'action':'store_true',
+            'default':False
+        },
+        {
+            'flag':'m',
+            'variable_name':'mhc_class',
+            'help':'switch the pipeline between MHC Class I and Class II, currently only Class I to which it defaults',
+            'action':'store',
+            'default':'class_i'
+        }
+        
+    ]
 
-    mhc_class = default_mhc_class
-    if args.mhc_class:
-        if args.mhc_class == 'class_ii':
-            mhc_class = 'class_ii' 
+    for argument in arguments:
+        parser.add_argument(f"-{argument['flag']}", f"--{argument['variable_name']}", help=argument['variable_name'], action=argument['action'])
 
-    all_data = default_data_level
-    if args.all_data:
-        all_data = 'all'
+    defaults = {argument['variable_name']:argument['default'] for argument in arguments}
+    parser.set_defaults(**defaults)
 
-    print (args)
+    kwargs = vars(parser.parse_args())
 
-    print (verbose)
-    print (force)
-    print (mode)
-    print (mhc_class)
-    print (all_data)
-
-    output = run_pipeline(verbose=verbose, force=force, mode=mode, mhc_class=mhc_class, all_data=all_data)
+    output = run_pipeline(**kwargs)
 
 
 if __name__ == '__main__':
