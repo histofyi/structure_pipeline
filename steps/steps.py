e8471eaaef2b20bdecf43394dd072aa9ace229fb
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-20 18:40:43 +0100

    Created a minimal steps dictionary

--- steps/steps.py
+++ steps/steps.py
@@ -1,4 +1,20 @@
-from rich.console import Console
-import argparse
+from test_step import test_step
 
-steps = {}
+steps = {
+    '1':{
+        'function':test_step,
+        'title_template':'Creating the folder structure in the output directory',
+        'list_item':'Creating the folder structure in the output directory',
+        'is_multi': False,
+        'multi_param': None,
+        'multi_options': None
+    },
+    '2':{
+        'function':test_step,
+        'title_template':'Creating the folder structure in the output directory',
+        'list_item':'Creating the folder structure in the output directory',
+        'is_multi': True,
+        'multi_param': 'pdb_code',
+        'multi_options': ['1hhh','1hhi','1hhj','1hhk']
+    }
+}
