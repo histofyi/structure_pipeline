e4b5f15d2228bb2c4161b67c96dc4de8a01156e4
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-24 19:03:12 +0100

    Refactored the pipeline step for updating the localpdb instance

--- steps/update_localpdb.py
+++ steps/update_localpdb.py
@@ -1,11 +1,31 @@
-from typing import Dict
+from typing import Dict, List
 
 from pipeline import get_current_time
 
-import os
+
+from subprocess import run
 import json
 
 import datetime 
+
+
+
+def parse_localpdb_output(raw_output:List) -> Dict:
+    if 'localpdb' in raw_output[0] and 'is up to date' in raw_output[0]:
+        updated = False
+    else:
+        updated = True
+
+    plugins = []
+
+    for line in raw_output:
+        if line.split(' ')[0] == 'Plugin':
+            plugins.append(line.split(' ')[1].strip().replace('\'','').lower())
+    
+    return {
+        'updated': updated,
+        'plugins': plugins
+    }
 
 
 def update_localpdb(**kwargs):
@@ -18,26 +38,44 @@ def update_localpdb(**kwargs):
     """
 
     config = kwargs['config']
+    verbose = kwargs['verbose']
 
     update_command = f"localpdb_setup -db_path {config['PATHS']['LOCALPDB_PATH']} --update"
 
-    os.system(update_command)
+    result = run(update_command, capture_output=True, shell=True, text=True)
+
+    if len(result.stderr) > 0:
+        success = False
+        errors = result.stderr
+    else:
+        success = True
+        if verbose:
+            print(result.stdout)
+            
+        raw_output = [line for line in result.stdout.splitlines() if len(line) > 0]
+
+
+        parsed_output = parse_localpdb_output(raw_output)
 
     status_log_path = f"{config['PATHS']['LOCALPDB_PATH']}/data/status.log"
     with open(status_log_path, "r") as status_logfile:
         version_info = json.load(status_logfile)
-        print (version_info)
 
     current_version = {}
     
     for version in version_info:
         if version_info[version][0] == 'OK':
             current_version = {
-                'version':version,
-                'last_updated':version_info[version][1],
-                'last_checked':get_current_time()
+                'current_version':version,
+                'updated_at':version_info[version][1],
+                'checked_at':get_current_time()
             }
-    action_log = {}
+    action_log = {
+        'version': current_version,
+        'updated': parsed_output['updated'],
+        'plugins': parsed_output['plugins']
+    }
+
     return action_log
 
 
