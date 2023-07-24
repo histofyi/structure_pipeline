50df4f01952407af3be3fdbe4a817b83595f2ca3
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-24 15:34:44 +0100

    Fixed typo

--- steps/steps.py
+++ steps/steps.py
@@ -3,8 +3,8 @@ from update_localpdb import update_localpdb
 steps = {
     '1':{
         'function':update_localpdb,
-        'title_template':'Updating the local copy of the pdb using localpbd.',
-        'list_item':'Updating the local copy of the pdb using localpbd.',
+        'title_template':'Updating the local copy of the pdb using localpdb.',
+        'list_item':'Updating the local copy of the pdb using localpdb.',
         'is_multi': False,
         'multi_param': None,
         'multi_options': None
