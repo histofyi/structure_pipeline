559084946ad7f52e2003c8ac38d411acb6f841f8
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-26 18:25:56 +0100

    Added type hints and docstrings

--- steps/fetch_stcrdab_info.py
+++ steps/fetch_stcrdab_info.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 import pandas as pd
 import os
 
@@ -18,20 +20,42 @@ chain_types = {
     ]
 }
 
-def get_tcr_pdbcode_list():
+def get_tcr_pdbcode_list() -> List:
+    """
+    This function queries STCRDab using pandas to generate a list of pdbcodes contained within the resource
+
+    Returns:
+      List: a list of pdb codes relating to structures in STCRDab
+    """
     df = pd.read_html('http://opig.stats.ox.ac.uk/webapps/stcrdab/Browser?all=true')[0]
     pdb_codes = pd.concat([df['PDB']])
     return pdb_codes
 
 
-def chain_set_template():
+def chain_set_template() -> Dict:
+  """
+  This function returns a default dictionary to describe a chainset for a specific TCR structure
+
+  Returns:
+    Dict: a default chain set dictionary
+  """
   return {
       'chains':[],
       'subgroup':''
   }
 
 
-def process_tcr_data(raw_tcr_dict, pdb_code):
+def process_tcr_data(raw_tcr_dict:Dict, pdb_code:str) -> Dict:
+  """
+  This function takes raw information on the TCR structure and creates a dictionary structure
+  
+  Args:
+      raw_tcr_dict (Dict): the output of the fetch_tcr_info function
+      pdb_code (str): the pdb code of the structure
+
+  Returns:
+      Dict: a structured dictionary of the data
+    """
   for row in raw_tcr_dict:
     clean_tcr_dict = {}
     if str(row['TCRtype']) != 'nan':
@@ -56,6 +80,15 @@ def process_tcr_data(raw_tcr_dict, pdb_code):
 
 
 def fetch_tcr_info(pdb_code):
+  """
+  This function downloads the summary information from STCRDab and generates a simple dictionary of the fields
+
+  Args:
+      pdb_code (str): the pdb_code for the structure
+
+  Returns:
+      Dict: a simple dictionary of values from STCRDab
+  """
   url = f'http://opig.stats.ox.ac.uk/webapps/stcrdab/summary/{pdb_code}'
   raw_tcr_df = pd.read_table(url, sep='\t')
   raw_tcr_dict = pd.concat([
@@ -77,7 +110,22 @@ def fetch_tcr_info(pdb_code):
 
 
 def fetch_stcrdab_info(**kwargs):
-    
+    """
+    This function retrieves the dataset of information about antibody structures from STCRDab
+
+    Args:
+        **kwargs: Arbitrary keyword arguments.
+
+    Returns
+        Dict: the action output for the step (a dictionary containing lists of pdb_codes for already cached, downloaded and errors)
+
+    Keyword Args:
+        config (Dict): the dictionary of configuration
+        verbose (bool): the verbosity of the output to the terminal
+        force (bool): whether the whole of the dataset should be refreshed   
+        output_path (str): the output path for the processed data
+        console (rich.console.Console) : the console instance from the pipeline class 
+    """
     config = kwargs['config']
     verbose = kwargs['verbose']
     force = kwargs['force']
