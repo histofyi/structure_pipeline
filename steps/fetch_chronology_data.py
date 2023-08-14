92b1a6e89c48bf73f71b30c61a3ef1536e2b7d91
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-08-14 13:18:03 +0100

    Refactored to append the changed structures to work_to be done
    # TODO check that this works when you have a full run done and there are revised structures

--- steps/fetch_chronology_data.py
+++ steps/fetch_chronology_data.py
@@ -1,6 +1,6 @@
 from typing import Dict, List, Tuple
 
-from rich.progress import Progress
+
 
 from pipeline import create_folder
 
@@ -14,6 +14,7 @@ from providers import PDBeProvider
 from datetime import datetime
 import os
 
+
 output_path = None
 output_facet = 'chronology'
 
@@ -25,7 +26,7 @@ def fetch_chronology_data_action(**action_args) -> Tuple[bool, Dict, List]:
     pdb_code = action_args['pdb_code']
 
     facet_path = get_facet_path(output_path, 'structures', output_facet, pdb_code)
-    
+
 
     data, success, errors = PDBeProvider(pdb_code).fetch_summary()
 
@@ -56,13 +57,29 @@ def fetch_chronology_data_action(**action_args) -> Tuple[bool, Dict, List]:
 def fetch_chronology_data(**kwargs):
     output_path = kwargs['output_path']
 
+    known_matches = read_json(f"assets/overrides/query_localpdb/known_matches.json")
+
     new_work = read_json(f"assets/overrides/query_localpdb/new_work.json")
 
-    new_work = new_work[0:10]
+    complete_work = known_matches.copy()
+
+    for structure in new_work:
+        if structure not in complete_work:
+            complete_work.append(structure)
 
     kwargs['force'] = True
 
-    # TODO build the logic around adding structures that need updating to the new_work list
-    print (needs_updating)
+    action_output = do_work(complete_work, fetch_chronology_data_action, output_facet, input_facet=None, kwargs=kwargs)
 
-    action_output = do_work(new_work, fetch_chronology_data_action, output_facet, input_facet=None, kwargs=kwargs)
+    # TODO build the logic around adding structures that need updating to the new_work list
+    print (f"There are {len(needs_updating)} structures that need to be updated due to revisions on the PBD.")
+
+    if len(needs_updating) > 0:
+        known_matches = read_json(f"assets/overrides/query_localpdb/known_matches.json")
+        structures_to_update = {structure['pdb_code']:structure for structure in known_matches if structure['pdb_code'] in needs_updating}
+        for pdb_code in needs_updating:
+            new_work.append(structures_to_update[pdb_code])
+
+    action_output['needs_updating'] = needs_updating
+
+    return action_output
